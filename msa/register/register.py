67c08fc9bf799f5ab292f19301db687bc3667871
hyowonlee <hyowonlee@hyowonleeui-MacBookPro.local>
2022-07-26 16:55:19 +0900

    Git Setting

---
+++ msa/register/register.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+import webbrowser
+import os
+from logging.config import dictConfig
+
+# Debug
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler', 
+            'filename': 'test_error.log',
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('register.html')
+    
+
+if __name__=="__main__":
+    app.run()
